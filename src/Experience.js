8bc869b24eb3129473c95a0d4d4b4961a732ffe3
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-23 15:14:10 +0100

    custom geometry

--- src/Experience.js
+++ src/Experience.js
@@ -1,6 +1,7 @@
 import { extend, useFrame, useThree } from '@react-three/fiber'
 import { useRef } from 'react'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import CustomObject from './CustomObject.js'
 
 extend({ OrbitControls: OrbitControls })
 
@@ -35,6 +36,7 @@ export default function Experience() {
                 <planeGeometry />
                 <meshStandardMaterial color='greenyellow' />
             </mesh>
+            <CustomObject />
         </>
     )
 }
